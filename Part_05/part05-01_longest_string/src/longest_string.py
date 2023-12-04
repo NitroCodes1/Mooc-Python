2e4c0a509aa4e80dbada3e05116eb6ba862a6d03
NitroCodes1 <152531460+NitroCodes1@users.noreply.github.com>
2023-12-04 20:30:15 +0000

    Python Mooc

---
+++ Part_05/part05-01_longest_string/src/longest_string.py
@@ -0,0 +1,11 @@
+def longest(list : list):
+    longest = list[0]
+    for word in list:
+        if len(word) > len(longest):
+            longest = word
+    return longest
+
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
